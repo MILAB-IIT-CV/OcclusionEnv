be29d91d75d5f47ec5bac23bae1efe1d9ed0451e
Mátyás Szántó <51972157+matyasszanto@users.noreply.github.com>
2021-12-22 22:51:48 +0100

    minor modifications to ShapeNet_unzipper.sh

--- ShapeNet_unzipper.sh
+++ ShapeNet_unzipper.sh
@@ -2,8 +2,8 @@
 # This script checks if ShapeNetCore data is downloaded to Matyi's external drive, and if yes, unzips it.
 
 #project specific output folder for data - added to gitignore to make sure it isn't available online
-dataset_dir="./data/shapenetcore"
-zip_dir="/Volumes/MacMiklos/M/BME"
+dataset_dir="/Volumes/MacMiklos/M/BME/2021_12-OcclusionEnvironment/Shapenet/"
+zip_dir="/Volumes/MacMiklos/M/BME/2021_12-OcclusionEnvironment/Shapenet/"
 #/2021_12\ -\ Occlusion\ Environment/Shapenet/"
 zip_file="ShapeNetCore.v2.zip"
 
@@ -12,7 +12,7 @@ zip_file="ShapeNetCore.v2.zip"
 # >> ln -s <path/to/ShapeNetCore/> shapenetcore
 
 cd "$zip_dir"
-
+pwd
 if [ -f $zip_file ];
 then
   echo "Zip file found."
