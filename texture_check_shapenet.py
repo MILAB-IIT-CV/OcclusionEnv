8470bc2f5154a81749dc9e2d74d768c9326b7fef
Mátyás Szántó <51972157+matyasszanto@users.noreply.github.com>
2022-02-22 20:44:51 +0100

    added multiple image display

--- texture_check_shapenet.py
+++ texture_check_shapenet.py
@@ -20,10 +20,56 @@ from pytorch3d.renderer import (
 from pytorch3d.structures import Meshes
 from torch.utils.data import DataLoader
 
+from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+
 # add path for demo utils functions
 import sys
 import os
 sys.path.append(os.path.abspath(''))
+
+def image_grid(
+    images,
+    rows=None,
+    cols=None,
+    fill: bool = True,
+    show_axes: bool = False,
+    rgb: bool = True,
+):
+    """
+    A util function for plotting a grid of images.
+    Args:
+        images: (N, H, W, 4) array of RGBA images
+        rows: number of rows in the grid
+        cols: number of columns in the grid
+        fill: boolean indicating if the space between images should be filled
+        show_axes: boolean indicating if the axes of the plots should be visible
+        rgb: boolean, If True, only RGB channels are plotted.
+            If False, only the alpha channel is plotted.
+    Returns:
+        None
+    """
+    if (rows is None) != (cols is None):
+        raise ValueError("Specify either both rows and cols or neither.")
+
+    if rows is None:
+        rows = len(images)
+        cols = 1
+
+    gridspec_kw = {"wspace": 0.0, "hspace": 0.0} if fill else {}
+    fig, axarr = plt.subplots(rows, cols, gridspec_kw=gridspec_kw, figsize=(15, 9))
+    bleed = 0
+    fig.subplots_adjust(left=bleed, bottom=bleed, right=(1 - bleed), top=(1 - bleed))
+
+    for ax, im in zip(axarr.ravel(), images):
+        if rgb:
+            # only render RGB channels
+            ax.imshow(im[..., :3])
+        else:
+            # only render Alpha channel
+            ax.imshow(im[..., 3])
+        if not show_axes:
+            ax.set_axis_off()
 
 
 if __name__ == '__main__':
@@ -34,7 +80,7 @@ if __name__ == '__main__':
     else:
         device = torch.device("cpu")
 
-    SHAPENET_PATH = "data/shapenetcore"
+    SHAPENET_PATH = "data/shapenet/shapenetcore"
     shapenet_dataset = ShapeNetCore(SHAPENET_PATH, version=2)
 
     print("DEBUG: dataset load complete")
@@ -63,6 +109,7 @@ if __name__ == '__main__':
     images_by_model_ids = shapenet_dataset.render(
         model_ids=[
             "2d7562f5bf2c7f2da1d85548168d6015",
+            "2d7562f5bf2c7f2da1d85548168d6015",
         ],
         device=device,
         cameras=cameras,
@@ -70,6 +117,8 @@ if __name__ == '__main__':
         lights=lights,
         shader_type=HardFlatShader
     )
+
+    image_grid(images_by_model_ids.cpu().numpy(), rows=1, cols=3, rgb=True)
 
     print('DEBUG: render complete')
 
