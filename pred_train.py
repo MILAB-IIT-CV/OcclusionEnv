0fad22a23814ec60339dfef9774022907ddba604
matyasszanto <matyasszanto@gmail.com>
2022-05-18 19:39:12 +0200

    Train works, tested on small database.

---
+++ pred_train.py
@@ -0,0 +1,149 @@
+from dataset import OcclusionDataset
+from torch.utils.data import DataLoader
+from model import PredictorNet
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.optim import lr_scheduler
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import sys
+import time
+
+if __name__ == '__main__':
+
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    batch_size = 128
+
+    trainSet = OcclusionDataset("./Dataset/", "train")
+    valSet = OcclusionDataset("./Dataset/", "val")
+
+    trainLoader = DataLoader(trainSet,
+                             batch_size=batch_size,
+                             shuffle=True,
+                             num_workers=4,
+                             )
+    valLoader = DataLoader(valSet,
+                           batch_size=batch_size,
+                           shuffle=True,
+                           num_workers=4,
+                           )
+
+    model = PredictorNet(8).cuda()
+
+    numEpochs = 3
+
+    criterion = nn.MSELoss()
+    optimizer = optim.AdamW(model.parameters(),
+                            lr=1e-4,
+                            weight_decay=1e-6,
+                            )
+    scheduler = lr_scheduler.CosineAnnealingLR(optimizer,
+                                               numEpochs,
+                                               1e-5,
+                                               )
+
+    losses = []
+    accs = []
+    vallosses = []
+    valaccs = []
+    best_loss = 1
+    best_acc = 0
+
+    for i in range(numEpochs):
+
+        running_loss = 0
+        running_acc = 0
+
+        for data in tqdm(trainLoader):
+
+            # Read dataset
+            img, _, grad, _ = data
+            img, grad = img.cuda(), grad.cuda()
+
+            # Calculate grad angle sin and cos
+            alpha = torch.nan_to_num(torch.arctan(grad[:, 0]/grad[:, 1]),
+                                     nan=0,
+                                     )
+
+            grad_alpha_sin, grad_alpha_cos = torch.sin(alpha).cuda(), torch.cos(alpha).cuda()
+
+            grad_alpha_gt = torch.empty((len(alpha), 2), dtype=torch.float64).cuda()
+
+            grad_alpha_gt[:, 0] = grad_alpha_sin
+            grad_alpha_gt[:, 1] = grad_alpha_cos
+
+            optimizer.zero_grad()
+
+            grad_pred = model(img)
+            loss = criterion(grad_pred.float(),
+                             grad_alpha_gt.float(),
+                             )
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        running_loss /= len(trainLoader)
+
+        print("Train epoch %d finished. Loss: %.6f, Accuracy: %.2f" % (i + 1,
+                                                                       running_loss,
+                                                                       running_acc,
+                                                                       )
+              )
+        sys.stdout.flush()
+
+        losses.append(running_loss)
+
+        running_loss = 0
+
+        for data in tqdm(valLoader):
+
+            img, _, grad, _ = data
+            img, grad = img.cuda(), grad.cuda()
+
+            # Calculate grad angle sin and cos
+            alpha = torch.nan_to_num(torch.arctan(grad[:, 0] / grad[:, 1]),
+                                     nan=0,
+                                     )
+
+            grad_alpha_sin, grad_alpha_cos = torch.sin(alpha).cuda(), torch.cos(alpha).cuda()
+
+            grad_alpha_gt = torch.empty((len(alpha), 2), dtype=torch.float64).cuda()
+
+            grad_alpha_gt[:, 0] = grad_alpha_sin
+            grad_alpha_gt[:, 1] = grad_alpha_cos
+
+            with torch.no_grad():
+                grad_pred = model(img)
+                loss = criterion(grad_pred.float(),
+                                 grad_alpha_gt.float(),
+                                 )
+
+                running_loss += loss.item()
+
+        running_loss /= len(valLoader)
+
+        print("Val epoch %d finished. Loss: %.6f, Accuracy: %.2f" % (i + 1,
+                                                                     running_loss,
+                                                                     running_acc
+                                                                     )
+              )
+        sys.stdout.flush()
+
+        vallosses.append(running_loss)
+
+        if running_loss < best_loss:
+            best_loss = running_loss
+            torch.save(model, "bestGradPredModel.pt")
+            print("Best model found and saved")
+
+        scheduler.step()
+
+    plt.plot(losses)
+    plt.plot(vallosses)
+    plt.show()
