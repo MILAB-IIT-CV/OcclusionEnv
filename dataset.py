9c65a10d11e092fddb3db4c3cc9764dde92e9ff1
Márton Szemenyei <szemenyei@iit.bme.hu>
2022-06-15 17:07:08 +0200

    replaced isnan function

--- dataset.py
+++ dataset.py
@@ -82,9 +82,10 @@ class OcclusionDataset(Dataset):
         label = torch.tensor(np.asarray(label)).float().unsqueeze(0)/255.0
 
         # Calculate grad angle sin and cos
-        alpha = torch.nan_to_num(torch.arctan(grad[0] / grad[1]),
-                                 nan=0,
-                                 )
+        with torch.no_grad():
+            alpha = torch.arctan(grad[0] / grad[1])
+            alpha[torch.isnan(alpha)] = 0
+
         grad = torch.tensor([torch.sin(alpha), torch.cos(alpha)]).float()
 
 
